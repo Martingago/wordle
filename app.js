62bc9491c7742253ee2817aed4b3ad253e2cad41
Martin <gagochorenmartin@gmail.com>
2022-06-11 23:11:15 +0200

    añadida funcionalidad teclado y animaciones

--- app.js
+++ app.js
@@ -18,17 +18,16 @@ const intentos = document.querySelectorAll(".palabra-usuario");
 let posicion = 0;
 
 // Cargar datos introducidos por usuario en pantalla
+document.addEventListener("keydown", (e) => {
 
-    document.addEventListener("keydown", (e) => {
-
-        if (validarInput(e.key) && introducidaUsuario.length <= 4) {
-            intentos[posicion].children[introducidaUsuario.length].textContent = `${e.key.toLowerCase()}`;
-            introducidaUsuario.push(e.key.toLowerCase());
-        } else if (e.key.toLowerCase() === "backspace") {
-            introducidaUsuario.pop();
-            intentos[posicion].children[introducidaUsuario.length].textContent = null;
-        }
-    });
+    if (validarInput(e.key) && introducidaUsuario.length <= 4 &&  posicion <5) {
+        intentos[posicion].children[introducidaUsuario.length].textContent = `${e.key.toLowerCase()}`;
+        introducidaUsuario.push(e.key.toLowerCase());
+    } else if (e.key.toLowerCase() === "backspace") {
+        introducidaUsuario.pop();
+        intentos[posicion].children[introducidaUsuario.length].textContent = null;
+    }
+});
 
 // Validar que el valor sea una letra
 const abecedario = "abcdefghijklmnñopqrstuvwxyzçABCDEFGHIJKLMNÑOPQRSTUVWXYZÇ";
@@ -41,45 +40,106 @@ const validarInput = (texto) => {
     return false;
 };
 
+// TECLADO  
+const tecladoLetra = document.querySelectorAll(".cargar-letra");
+const cargarDatosTeclado = () => {
+    for (let i = 0; i < tecladoLetra.length; i++) {
+        tecladoLetra[i].addEventListener(
+            "click",
+            () => {
+                if (introducidaUsuario.length <= 4 && posicion <5) {
+                    intentos[posicion].children[introducidaUsuario.length].textContent = `${tecladoLetra[i].textContent}`;
+                    introducidaUsuario.push(tecladoLetra[i].textContent.toLowerCase())
+                }
+            }
+        )
+    }
+}
+
+
+const borrarDatosTeclado = () => {
+    const borrarLetra = document.querySelector(".borrar-letra");
+    borrarLetra.addEventListener(
+        "click",
+        () => {
+            introducidaUsuario.pop();
+            intentos[posicion].children[introducidaUsuario.length].textContent = null;
+
+        }
+    );
+}
+
+const enviarDatosTeclado = () => {
+    const enterTeclado = document.querySelector(".cargar-palabra");
+    enterTeclado.addEventListener("click",
+        () => {
+            inputToString()
+            validarPalabra()
+        });
+
+
+}
+
+borrarDatosTeclado();
+cargarDatosTeclado();
+enviarDatosTeclado()
+
 // Convertir el input del usuario en string
 let letraString = "";
 const inputToString = () => {
     letraString = introducidaUsuario.join("");
 };
 
-// Validacion de las palabras que introduce el usuario
 
 const pantallaError = document.querySelector(".error-msg");
 
 document.addEventListener("keydown", (e) => {
     if (e.key.toLowerCase() === "enter") {
         inputToString();
-        if (introducidaUsuario.length === 5) {
-            if (basePalabras.includes(letraString)) {
-                comprobarVictoria();
-                posicion++;
-                introducidaUsuario = []
-                
-            } else {
-                pintarPantalla("La palabra no es válida");
-            }
-        } else {
-            pintarPantalla("La palabra no tiene 5 letras");
-        }
+        validarPalabra();
     }
 });
+
+
+// Validacion de las palabras que introduce el usuario;
+// Si la palabra es valida, se comprueba
+const validarPalabra = () => {
+    if (introducidaUsuario.length === 5) {
+        if (basePalabras.includes(letraString)) {
+            comprobarVictoria();
+            posicion++;
+            introducidaUsuario = []
+
+        } else {
+            pintarPantalla("La palabra no es válida");
+            zumbidoError()
+        }
+    } else {
+        pintarPantalla("La palabra no tiene 5 letras");
+        zumbidoError()
+    }
+}
 
 const pintarPantalla = (texto) => {
     pantallaError.textContent = texto;
 };
+
+const zumbidoError = () => {
+    intentos[posicion].style.animation = "palabraNoValida .3s linear";
+    setTimeout(() => {
+        intentos[posicion].style.animation = "";
+    }, 300);
+}
+
 
 // intento perdido
 
 let vidas = 5;
 const perderIntento = () => {
     vidas--;
-    if (vidas <= 0) {
+    if (vidas < 0) {
         pintarPantalla("te has quedado sin vidas");
+        return vidas
     } else {
         comprobarLetra();
     }
@@ -88,6 +148,7 @@ const comprobarVictoria = () => {
     if (letraString == secretWord) {
         pintarPantalla("Ganaste");
         comprobarLetra()
+
     } else {
         perderIntento();
     }
@@ -95,18 +156,40 @@ const comprobarVictoria = () => {
 
 // Pista usuario letras
 
+const letrasCorrecta = [];
+const letrasExiste =  [];
+const letrasNoExiste = [];
+
+
 const comprobarLetra = () => {
     for (let i = 0; i < 5; i++) {
         if (descompuesta[i] == introducidaUsuario[i]) {
-            intentos[posicion].children[i].classList.add("correcta")
-        } else {
+            intentos[posicion].children[i].classList.add("correcta");
+            letrasCorrecta.push(introducidaUsuario[i])
+            
+        } 
+        
+        else {
             if (descompuesta.includes(introducidaUsuario[i])) {
+                intentos[posicion].children[i].classList.add("existe")
+                letrasExiste.push(introducidaUsuario[i])
+            
+
                 // crear funcion en caso de: una palabra introducida por un usuario que contiene una letra duplicada,
                 // si solo se encuentra 1 vez, pintar la que se encuentra mal situada en posicion "noexiste", ya que la que existe ya se ha pintado
-                intentos[posicion].children[i].classList.add("existe")
+
             } else {
                 intentos[posicion].children[i].classList.add("noexiste")
+                letrasNoExiste.push(introducidaUsuario[i])
+            
             }
         }
+        
     }
+    console.log("letras correctas:", letrasCorrecta)
+    console.log("letras existen:", letrasExiste)
+    
 };
+
+
+
